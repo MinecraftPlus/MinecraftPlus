4df8552e903c38d60ebf68c822de47d0db152f0d
theWituch <thewituch@gmail.com>
2022-01-13 17:05:42 +0100

    Upgrade SplitJar task to generate 3 outputs, sources, resources and libraries

--- buildSrc/src/main/groovy/org/minecraftplus/gradle/SplitJar.groovy
+++ buildSrc/src/main/groovy/org/minecraftplus/gradle/SplitJar.groovy
@@ -1,7 +1,11 @@
 package org.minecraftplus.gradle
 
+import net.minecraftforge.mcpconfig.tasks.Utils
+import net.minecraftforge.srgutils.IMappingFile
 import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 
@@ -15,20 +19,35 @@ import java.util.zip.ZipOutputStream
  */
 public class SplitJar extends DefaultTask {
     @InputFile input
+    @InputFile mappings
+    @Input acceptable = []
     @OutputFile sources
     @OutputFile resources
+    @OutputFile libraries
 
     @TaskAction
     def exec() {
+        Utils.init()
+        def classes = IMappingFile.load(mappings).classes.collect{ it.original + '.class' }.toSet()
+
         new ZipOutputStream(sources.newOutputStream()).withCloseable{ so ->
             new ZipOutputStream(resources.newOutputStream()).withCloseable{ ro ->
-                new ZipInputStream(input.newInputStream()).withCloseable{ jin ->
-                    for (def entry = jin.nextEntry; entry != null; entry = jin.nextEntry) {
-                        def out = entry.name.endsWith(".class") ? so : ro
-                        def oentry = new ZipEntry(entry.name)
-                        oentry.lastModifiedTime = entry.lastModifiedTime
-                        out.putNextEntry(oentry)
-                        out << jin
+                new ZipOutputStream(libraries.newOutputStream()).withCloseable { lo ->
+                    new ZipInputStream(input.newInputStream()).withCloseable { jin ->
+                        for (def entry = jin.nextEntry; entry != null; entry = jin.nextEntry) {
+                            def out = null
+                            if (classes.contains(entry.name)) {
+                                out = so
+                            } else if (acceptable.stream().anyMatch(it -> entry.name.startsWith(it as String))) {
+                                out = entry.name.endsWith(".class") ? so : ro
+                            } else
+                                out = lo // library
+
+                            def oentry = new ZipEntry(entry.name)
+                            oentry.lastModifiedTime = entry.lastModifiedTime
+                            out.putNextEntry(oentry)
+                            out << jin
+                        }
                     }
                 }
             }
