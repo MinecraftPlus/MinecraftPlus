95e5569f1cf32517f7a6794d01c42880015279fa
theWituch <thewituch@gmail.com>
2022-01-26 17:47:04 +0100

    New repository action tasks implementation

--- buildSrc/src/main/groovy/org/minecraftplus/gradle/InitRepository.groovy
+++ buildSrc/src/main/groovy/org/minecraftplus/gradle/InitRepository.groovy
@@ -3,14 +3,9 @@ package org.minecraftplus.gradle
 import org.eclipse.jgit.api.Git
 import org.eclipse.jgit.errors.RepositoryNotFoundException
 import org.gradle.api.DefaultTask
-import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputDirectory
-import org.gradle.api.tasks.Optional
-import org.gradle.api.tasks.OutputDirectory
-import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
-
 /**
  * Initializes project repository
  */
@@ -18,7 +13,6 @@ class InitRepository extends DefaultTask {
 
     @InputDirectory File target
     @Input String branchName = "master"
-    @OutputFile lock
 
     @TaskAction
     def exec() {
@@ -26,9 +20,7 @@ class InitRepository extends DefaultTask {
 
         // Check if repository is already created
         try (Git git = Git.open(target)) {
-            logger.lifecycle(" - repository exist {}",
-                    lock.exists() ? "and lock file found" : "")
-
+            logger.lifecycle(" - repository exist")
             return // Nothing to do
         } catch (RepositoryNotFoundException e) {}
 
@@ -46,12 +38,10 @@ class InitRepository extends DefaultTask {
             logger.lifecycle("Staged all files into repository")
 
             git.commit()
+                    .setAll(true)
                     .setMessage("Initial commit")
                     .call()
             logger.lifecycle("Committed changes to repository")
-
-            lock.createNewFile()
-            logger.lifecycle("Created '{}' lock file", ".freshinit")
         }
     }
 }
--- buildSrc/src/main/groovy/org/minecraftplus/gradle/SyncChanges.groovy
+++ buildSrc/src/main/groovy/org/minecraftplus/gradle/SyncChanges.groovy
@@ -1,39 +1,45 @@
 package org.minecraftplus.gradle
 
 import org.eclipse.jgit.api.Git
-import org.eclipse.jgit.errors.RepositoryNotFoundException
-import org.eclipse.jgit.lib.ObjectId
+import org.eclipse.jgit.api.MergeResult
+import org.eclipse.jgit.lib.RepositoryState
 import org.eclipse.jgit.merge.ContentMergeStrategy
 import org.eclipse.jgit.merge.MergeStrategy
-import org.eclipse.jgit.revwalk.RevCommit
-import org.eclipse.jgit.revwalk.RevWalk
 import org.eclipse.jgit.transport.RemoteConfig
 import org.eclipse.jgit.transport.URIish
 import org.gradle.api.DefaultTask
+import org.gradle.api.GradleException
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputDirectory
-import org.gradle.api.tasks.InputFile
-import org.gradle.api.tasks.Optional
+import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.TaskAction
-
-import java.util.function.Function
-
 /**
  * Fetches changes from upstream and merges it
  */
 class SyncChanges extends DefaultTask {
 
-    @InputDirectory def File target
-    @Optional @InputFile def File freshinit
+    @InputDirectory File target
     @Input String remoteName
     @Input String remoteUrl
     @Input String localBranch = "master"
     @Input String remoteBranch = "master"
     @Input String message = "Synchronisation"
 
+    @Internal
+    File lockfile
+
     @TaskAction
     def exec() {
+        lockfile = new File(target, ".synchronized")
+
         try (Git git = Git.open(target)) {
+            def repository = git.getRepository()
+
+            // Checkout local master branch
+            def checkout = git.checkout()
+                    .setName(localBranch)
+                    .call()
+            logger.lifecycle("Checked out local branch '{}'", localBranch)
 
             def remotes = git.remoteList().call()
             if (remotes.stream().map(RemoteConfig::getName).noneMatch(r -> r == remoteName)) {
@@ -41,67 +47,123 @@ class SyncChanges extends DefaultTask {
                         .setName(remoteName)
                         .setUri(new URIish(remoteUrl))
                         .call()
-                logger.lifecycle("Adding remote '{}' from '{}'", remoteName, remoteUrl)
+                logger.lifecycle("Added remote '{}' from '{}'", remoteName, remoteUrl)
             }
 
-            git.fetch()
+            // Check if remote has requested branch
+            def remoteRefs = git.lsRemote()
                     .setRemote(remoteName)
-                    .setInitialBranch(remoteBranch)
+                    .call()
+            def remoteRef = remoteRefs.stream()
+                    .filter(ref -> ref.getName() == "refs/heads/" + remoteBranch)
+                    .findFirst().orElse(null)
+            if (remoteRef == null) {
+                throw new GradleException("Remote repository '${remoteUrl}' does not contain branch '${remoteBranch}'")
+            }
+
+            // Configure repository
+            def config = repository.getConfig()
+            // to to only fetch 'master' from from remote branches
+            config.setString("remote", remoteName, "fetch",
+                    "+refs/heads/${remoteBranch}:refs/remotes/${remoteName}/${remoteBranch}")
+//            // merge driver to process .gitattributes
+//            if (!config.getBoolean("merge", "ours", "driver", false)) {
+//                config.setBoolean("merge", "ours", "driver", true)
+//            }
+            config.save()
+
+            // Fetch from remote
+            def fetch = git.fetch()
+                    .setRemote(remoteName)
                     .call()
             logger.lifecycle("Fetched '{}' branch from remote '{}'", remoteBranch, remoteName)
 
-            git.checkout()
-                    .setName(localBranch)
-                    .call()
-            logger.lifecycle("Checked out local branch '{}'", localBranch)
+            def inSync = lockfile?.exists()
+            logger.lifecycle("Were synchronized: {}", inSync)
 
-            ObjectId localRef = git.getRepository().resolve(localBranch)
-            logger.lifecycle("     local '{}'", localRef.getName())
+            def state = repository.getRepositoryState();
+            logger.lifecycle("Repository in state: {}", state.toString())
+            switch(state) {
+                case RepositoryState.SAFE: {
+                    // On first repository sync we need 'ours' strategy when syncing
+                    def mergeStrategy = inSync
+                            ? MergeStrategy.RECURSIVE : MergeStrategy.OURS
+                    def contentStrategy = inSync
+                            ? ContentMergeStrategy.CONFLICT : ContentMergeStrategy.OURS
 
-            ObjectId upstreamRef = git.getRepository()
-                    .resolve(remoteName + "/" + remoteBranch)
-            logger.lifecycle("  upstream '{}'", upstreamRef.getName())
+                    logger.lifecycle("Merging changes in '{}' from remote '{}' to local '{}'",
+                            remoteBranch, remoteName, localBranch)
+                    logger.lifecycle(" - using merge/content strategy: {}/{}",
+                            mergeStrategy.getName(), contentStrategy.toString())
 
-            RevWalk revWalk = new RevWalk(git.getRepository())
-            for (RevCommit parent : revWalk.parseCommit(localRef).getParents()) {
-                if (upstreamRef.equals(parent)) {
-                    logger.lifecycle("Remote branch '{}' has already merged in local branch '{}'", remoteBranch, localBranch)
-                    return
+                    def merge = git.merge()
+                            .include(remoteRef)
+                            .setMessage(message)
+                            .setStrategy(mergeStrategy)
+                            .setContentMergeStrategy(contentStrategy)
+                            .setCommit(false) // need no commit to add lock file
+                            .call()
+                    def status = merge.getMergeStatus()
+                    logger.lifecycle("Merge status: {}", status)
+
+//                    TODO: Maybe replace merge with pull?
+//                    def pull = git.pull()
+//                            .setRemote(remoteName)
+//                            .setRemoteBranchName(remoteBranch)
+//                            .setStrategy(strategy)
+//                            .setContentMergeStrategy(contentStrategy)
+//                            .call()
+//                    logger.lifecycle("Pull fetch status: {}", pull.getFetchResult())
+//                    logger.lifecycle("Pull merge status: {}", pull.getMergeResult())
+
+                    if (!status.isSuccessful()) {
+                        switch (status) {
+                            case MergeResult.MergeStatus.CONFLICTING:
+                                throw new GradleException("Merge results in content conflicts [" +  merge.getConflicts().size() + "]")
+                            default:
+                                throw new GradleException("Merge fails! [" + status + "]")
+                        }
+                    } else {
+                        switch (status) {
+                            case MergeResult.MergeStatus.ALREADY_UP_TO_DATE:
+                                logger.lifecycle("Remote branch '{}' has already merged in local branch '{}'", remoteBranch, localBranch)
+                                return
+                            default:
+                                break
+                        }
+                    }
+
+                    // no break! If successfully then fallthrough to next case step
                 }
+                case RepositoryState.MERGING_RESOLVED: {
+                    // Create a lock file to determine if your repositories have already been synced
+                    if (!lockfile.exists()) {
+                        createLockFile()
+                        git.add().addFilepattern(lockfile.getName()).call()
+                        logger.lifecycle("Created '{}' lock file", lockfile.getName())
+                    }
+
+                    // Commit results
+                    def result = git.commit()
+                            .call()
+                    logger.lifecycle("Committed changes as '{}'", result.getFullMessage())
+                    break;
+                }
+                case RepositoryState.MERGING: {
+                    throw new GradleException("Resolve merge conflicts in repository before resume syncing!")
+                }
+                default:
+                    throw new IllegalStateException("Cannot be here, ups!")
             }
-
-            // On fresh repositories we need ours when syncing, else recursive
-            def strategy = freshinit?.exists()
-                    ? MergeStrategy.OURS : MergeStrategy.RECURSIVE
-
-//            logger.lifecycle("Fresh file {}", freshinit?.toString())
-//            logger.lifecycle("Fresh exists {}", freshinit?.exists())
-//            logger.lifecycle("Fresh size {}", freshinit?.size())
-//            logger.lifecycle("Strategy  {}", strategy)
-
-            git.merge().include(upstreamRef)
-                    .setMessage(message)
-                    .setStrategy(strategy)
-                    .setContentMergeStrategy(ContentMergeStrategy.OURS)
-//                    .setCommit(false)
-                    .call()
-            logger.lifecycle("Merged changes from '{}' remote '{}' with strategy '{}'",
-                    remoteBranch, remoteName, strategy.getName())
-
-            if (freshinit?.exists()) {
-                freshinit.delete()
-//            git.add()
-//                    .addFilepattern(freshinit.getName())
-//                    .call()
-                logger.lifecycle("Removed '{}' lock file", freshinit.getName())
-            }
-
-//            git.commit()
-//                    .setMessage(message)
-//                    .call()
-//            logger.lifecycle("Committed changes '{}'", message)
         } catch (Exception e) {
             throw e
         }
+    }
+
+    def createLockFile() {
+        lockfile.createNewFile()
+        lockfile.append("DON'T DELETE THIS FILE!!!)\r\n\r\n")
+        lockfile.append("This file is used to check if distro repository is in synchronization with upstream branch\r\n\r\n")
+        lockfile.append("DON'T DELETE THIS FILE!!!)\r\n")
     }
 }
