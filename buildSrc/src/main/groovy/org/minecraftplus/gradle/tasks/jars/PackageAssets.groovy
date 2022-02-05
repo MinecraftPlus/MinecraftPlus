952682b9879833c1a23c75ecb4c6d832833c7cf0
theWituch <thewituch@gmail.com>
2022-02-05 07:30:00 +0100

    Add RESOURCES project to build

--- buildSrc/src/main/groovy/org/minecraftplus/gradle/tasks/jars/PackageAssets.groovy
+++ buildSrc/src/main/groovy/org/minecraftplus/gradle/tasks/jars/PackageAssets.groovy
@@ -1,21 +1,21 @@
 package org.minecraftplus.gradle.tasks.jars
 
+import net.minecraftforge.mcpconfig.tasks.SingleFileOutput
 import net.minecraftforge.mcpconfig.tasks.Utils
-import org.gradle.api.DefaultTask
 import org.gradle.api.GradleException
 import org.gradle.api.tasks.InputDirectory
 import org.gradle.api.tasks.InputFile
-import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 
+import java.nio.file.Files
+import java.nio.file.attribute.BasicFileAttributes
 import java.util.zip.ZipEntry
 import java.util.zip.ZipOutputStream
 
-class PackageAssets extends DefaultTask {
+class PackageAssets extends SingleFileOutput {
 
     @InputFile json
     @InputDirectory input
-    @OutputFile def dest
 
     @TaskAction
     def exec() {
@@ -37,7 +37,12 @@ class PackageAssets extends DefaultTask {
                 if (!file.exists() || !file.isFile())
                     throw new GradleException("Cannot find asset '" + key + "'")
 
-                output.putNextEntry(new ZipEntry(name))
+                def filePath = file.toPath()
+                def fileAttr = Files.readAttributes(filePath, BasicFileAttributes.class);
+
+                def entry = new ZipEntry('assets/' + name)
+                entry.lastModifiedTime = fileAttr.lastModifiedTime()
+                output.putNextEntry(entry)
                 def buffer = new byte[file.size()]
                 file.withInputStream {
                     output.write(buffer, 0, it.read(buffer))
