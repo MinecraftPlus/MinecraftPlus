13d5b961c80ddef64a90d5fbd8d3878e7897fc80
theWituch <thewituch@gmail.com>
2022-02-07 06:08:03 +0100

    Tasks tree fixup

--- buildSrc/src/main/groovy/org/minecraftplus/gradle/tasks/mapping/ConvertMappings.groovy
+++ buildSrc/src/main/groovy/org/minecraftplus/gradle/tasks/mapping/ConvertMappings.groovy
@@ -1,14 +1,12 @@
 package org.minecraftplus.gradle.tasks.mapping
 
 import net.minecraftforge.mcpconfig.tasks.SingleFileOutput
-import org.gradle.api.*
-import org.gradle.api.tasks.*
+import net.minecraftforge.srgutils.IMappingFile
+import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.TaskAction
+import org.minecraftplus.gradle.tasks.Utils
 
-import java.util.zip.*
-
-import net.minecraftforge.srgutils.*
-
-public class ConvertMappings extends SingleFileOutput {
+class ConvertMappings extends SingleFileOutput {
     @InputFile File input
 
     @TaskAction
