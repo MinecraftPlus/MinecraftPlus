bf49ca5fd97d223142f558553fbf886ae34b9789
theWituch <thewituch@gmail.com>
2022-02-07 04:13:50 +0100

    Move ConvertMappings task to mapping package

--- buildSrc/src/main/groovy/org/minecraftplus/gradle/tasks/mapping/ConvertMappings.groovy
+++ buildSrc/src/main/groovy/org/minecraftplus/gradle/tasks/mapping/ConvertMappings.groovy
@@ -1,5 +1,6 @@
-package net.minecraftforge.mcpconfig.tasks
+package org.minecraftplus.gradle.tasks.mapping
 
+import net.minecraftforge.mcpconfig.tasks.SingleFileOutput
 import org.gradle.api.*
 import org.gradle.api.tasks.*
 
