0c6c6ef20ee86e829addce5973450fba7306845d
theWituch <thewituch@gmail.com>
2022-03-19 19:05:06 +0100

    Get version manifest inheritance info directly from parent version manifest

--- buildSrc/src/main/groovy/org/minecraftplus/gradle/tasks/meta/GenerateVersionManifest.groovy
+++ buildSrc/src/main/groovy/org/minecraftplus/gradle/tasks/meta/GenerateVersionManifest.groovy
@@ -4,6 +4,7 @@ import groovy.json.JsonBuilder
 import groovy.json.JsonOutput
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.OutputDirectory
@@ -14,8 +15,8 @@ import java.security.MessageDigest
 
 class GenerateVersionManifest extends DefaultTask {
 
+    @InputFile File parent
     @Input String id
-    @Input String inherit
     @Input String type
     @Optional @Input Date time = new Date()
     @Optional @Input Date releaseTime = new Date()
@@ -34,6 +35,8 @@ class GenerateVersionManifest extends DefaultTask {
     def exec() {
         Utils.init()
 
+        def parentManifest = parent.json
+
         // Prepare downloads
         def downloads = [:]
         downloadFiles.each { name, file ->
@@ -49,7 +52,7 @@ class GenerateVersionManifest extends DefaultTask {
         def json = new JsonBuilder()
         json(
                 id: id,
-                inheritsFrom: inherit,
+                inheritsFrom: parentManifest.id,
                 time: time,
                 releaseTime: releaseTime,
                 type: type,
@@ -57,7 +60,10 @@ class GenerateVersionManifest extends DefaultTask {
                 complianceLevel: complianceLevel
         )
 
+        // Short manifest, with inheritance
         new File(output, id + '.json').text = JsonOutput.prettyPrint(json.toString())
+        // Full manifest, with overrided values
+        new File(output, id + '_full.json').text = new JsonBuilder(parentManifest + json.getContent()).toPrettyString()
     }
 
     // See https://stackoverflow.com/a/43082685
