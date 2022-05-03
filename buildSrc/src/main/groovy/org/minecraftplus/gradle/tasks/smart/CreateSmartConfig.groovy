f016889063a98a9c13c813422d8d61341eafce6b
theWituch <thewituch@gmail.com>
2022-05-03 18:32:51 +0200

    Add task generating SmartPatches config

---
+++ buildSrc/src/main/groovy/org/minecraftplus/gradle/tasks/smart/CreateSmartConfig.groovy
@@ -0,0 +1,32 @@
+package org.minecraftplus.gradle.tasks.smart
+
+import groovy.json.JsonBuilder
+import groovy.json.JsonOutput
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Internal
+import org.gradle.api.tasks.Optional
+import org.gradle.api.tasks.TaskAction
+import org.minecraftplus.gradle.tasks.Utils
+
+class CreateSmartConfig extends DefaultTask {
+
+    @Input String branch
+    @Optional @Input String lastcommit = null
+
+    @Internal File output
+
+    @TaskAction
+    def exec() {
+        Utils.init()
+
+        // Generate JSON
+        def json = new JsonBuilder()
+        json(
+            branch: branch,
+            lastcommit: lastcommit,
+        )
+
+        output.text = JsonOutput.prettyPrint(json.toString())
+    }
+}
