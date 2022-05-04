3644ad8bfed5c115b7fe663eeb7de3c469f42eb5
theWituch <thewituch@gmail.com>
2022-05-04 13:43:17 +0200

    Add task for generating patches from SmartPatches commit list

---
+++ buildSrc/src/main/groovy/org/minecraftplus/gradle/tasks/smart/SmartMakePatches.groovy
@@ -0,0 +1,65 @@
+package org.minecraftplus.gradle.tasks.smart
+
+import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.lib.RepositoryState
+import org.gradle.api.InvalidUserDataException
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputDirectory
+import org.gradle.api.tasks.TaskAction
+import uk.jamierocks.propatcher.task.MakePatchesTask
+
+class SmartMakePatches extends MakePatchesTask {
+
+    @Input String branch
+    @Input String commitId
+    @InputDirectory File gitrepo
+
+    @TaskAction
+    void doTask() {
+
+        try (Git git = Git.open(gitrepo)) {
+            def repository = git.getRepository()
+
+            // Check repository state - we need to be in safe checked-out state
+            def state = repository.getRepositoryState();
+            logger.trace("Repository in state: {}", state.toString())
+            switch(state) {
+                case RepositoryState.SAFE: {
+                    break
+                }
+                default:
+                    throw new IllegalStateException("Repository need be in safe state!")
+            }
+
+            // Check actual branch - we need be in branch from smart config
+            def actualBranch = repository.getBranch()
+            logger.trace("Actual branch: {}", actualBranch)
+            if (actualBranch != branch) {
+                throw new IllegalStateException("Target repository need be checked-out on branch " + branch)
+            }
+
+            // Checkout to specified commit and generate smartpatches
+            // After process repository will be checked out to starting position
+            try {
+                // Checkout smart commit id
+                git.checkout().setName(commitId).call();
+                makePatches()
+                logger.lifecycle("Patches maked in {}", patches)
+            } finally {
+                git.checkout().setName(actualBranch).call();
+            }
+        } catch (Exception e) {
+            throw e
+        }
+    }
+
+    void makePatches() {
+        if (!patches.exists())
+            patches.mkdirs()
+
+        def root = rootZip == null ? rootDir : rootZip
+        if (root == null)
+            throw new InvalidUserDataException("At least one of rootZip and rootDir has to be specified!")
+        process(root, target) // Make the patches
+    }
+}
