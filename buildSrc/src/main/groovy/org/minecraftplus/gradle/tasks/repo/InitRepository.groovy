96cf25a22f181e465956eb50c99c9311e26a2706
theWituch <thewituch@gmail.com>
2022-05-07 21:29:42 +0200

    Improve InitRepository task with additionalBranches argument and branch list checks
    It allows creating additional branches upon repository create.
    When repository exists task checks if all needed branches existing

--- buildSrc/src/main/groovy/org/minecraftplus/gradle/tasks/repo/InitRepository.groovy
+++ buildSrc/src/main/groovy/org/minecraftplus/gradle/tasks/repo/InitRepository.groovy
@@ -1,10 +1,13 @@
 package org.minecraftplus.gradle.tasks.repo
 
 import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.api.errors.RefAlreadyExistsException
+import org.eclipse.jgit.api.errors.RefNotFoundException
 import org.eclipse.jgit.errors.RepositoryNotFoundException
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputDirectory
+import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.TaskAction
 /**
  * Initializes project repository
@@ -13,6 +16,9 @@ class InitRepository extends DefaultTask {
 
     @InputDirectory File target
     @Input String branchName = "master"
+    @Input String commitMessage = "Initial commit"
+
+    @Optional @Input String[] additionalBranches = []
 
     @TaskAction
     def exec() {
@@ -20,7 +26,32 @@ class InitRepository extends DefaultTask {
 
         // Check if repository is already created
         try (Git git = Git.open(target)) {
-            logger.lifecycle(" - repository exist")
+            logger.lifecycle(" - repository exist", target)
+
+            //Check if repository already contains all needed branches
+            def branches = git.branchList().call().collect { ref ->
+                ref.name.split('/').last()
+            }
+            logger.lifecycle(" - contains branches {}", branches)
+
+            def needed = additionalBranches.plus(branchName).toUnique().toList()
+            def missing = []
+
+            logger.lifecycle("Checking branches needed: {}", needed)
+            def allPresent = needed.every { bname ->
+                if (branches.contains(bname)) {
+                    return true
+                } else {
+                    missing.add(bname)
+                    return false
+                }
+            }
+
+            // If any branch is missing throw an exception because it can be fixed only manually yet
+            if (!allPresent) {
+                throw new RefNotFoundException("A branch is missing in repository: ${missing}")
+            }
+
             return // Nothing to do
         } catch (RepositoryNotFoundException e) {}
 
@@ -30,6 +61,8 @@ class InitRepository extends DefaultTask {
                 .setInitialBranch(branchName)
                 .call()
         ) {
+            def repository = git.getRepository()
+
             logger.lifecycle("Created repository")
 
             git.add()
@@ -39,9 +72,17 @@ class InitRepository extends DefaultTask {
 
             git.commit()
                     .setAll(true)
-                    .setMessage("Initial commit")
+                    .setMessage(commitMessage)
                     .call()
             logger.lifecycle("Committed changes to repository")
+
+            // Create additional branch for other purposes
+            additionalBranches.each { bName ->
+                try {
+                    def bRef = git.branchCreate().setName(bName).call()
+                    logger.lifecycle("Created additional branch: {}", bRef.name)
+                } catch (RefAlreadyExistsException e) {}
+            }
         }
     }
 }
